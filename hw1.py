4fc5872ac4f7103a97ff598aeb51de5db49098ab
Ryeongeun-Kim <127407431+Ryeongeun-Kim@users.noreply.github.com>
2023-03-13 13:49:58 +0900

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -2,15 +2,10 @@ def get_radius():
     r = int(input('넓이를 구하고자 하는 원의 반지름은?'))
     return r
 
-get_radius
-result = get_radius()
-print('반지름', result,'인 원의 넓이 = 3.14 x', result, 'x', result,'=', result*result*3.14)
-
-
-def get_circle_area(num):
-  area = num*num*3.14
+def get_circle_area(s):
+  area = s*s*3.14
   return area
 
-input_num = int(input('넓이를 구하고자 하는 원의 반지름은?'))
-result = get_circle_area(input_num)
+s = get_radius()
+result = get_circle_area(s)
 print('원의 넓이는 :', result)
